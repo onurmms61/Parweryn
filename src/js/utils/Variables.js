176278ae6257c3d901d3db5abfaa19cdd9d1d130
PARS_TURK <nsarslan@hotmail.com.tr>
2018-05-20 14:13:57 +0300

    First Upload

---
+++ src/js/utils/Variables.js
@@ -0,0 +1,94 @@
+class Variables {
+
+  static get boxType() {
+    return "_-z1x";
+  }
+
+  static get attackerId() {
+    return "_-53b";
+  }
+
+  static get attackHp() {
+    return "_-84X";
+  }
+
+  static get attackShd() {
+    return "_-23t";
+  }
+
+  static get attackedId() {
+    return "_-01e";
+  }
+
+  static get moveDuration() {
+    return "_-m4x";
+  }
+
+  static get shipDestoyedId() {
+    return "_-U3J";
+  }
+
+  static get heroInitMaxHp() {
+    return "_-gE";
+  }
+
+  static get heroInitMaxShd() {
+    return "_-kU";
+  }
+
+  static get heroInitHp() {
+    return "_-33H";
+  }
+
+  static get hpUpdateMaxHp() {
+    return "_-V3D";
+  }
+
+  static get hpUpdateHp() {
+    return "_-z2T";
+  }
+
+  static get selectMaxHp() {
+    return "_-V3D";
+  }
+
+  static get selectMaxShd() {
+    return "_-kU";
+  }
+
+  static get selectHp() {
+    return "_-z2T";
+  }
+  
+   static get clanDiplomacy() {
+    return "_-L4b";
+  }
+
+  static get resource() {
+    return "_-6W";
+  }
+
+  static get resourceType() {
+    return "_-O4Y";
+  }
+
+  static get gateType() {
+    return "_-Ms";
+  }
+
+  static get gateId() {
+    return "_-km";
+  }
+
+  static get assetCreateX() {
+    return "_-83W";
+  }
+
+  static get assetCreateY() {
+    return "_-822";
+  }
+
+  static get battlestationClanDiplomacy() {
+    return "_-c3k";
+  }
+}
